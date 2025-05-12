cc2af165d32540fc4e1b376934494c3d410acb7a
DavidYVilla <dvilladuran@gmail.com>
2025-05-12 10:50:25 -0400

    Primer commit - configuracion inicial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    name: "my-eslint-config",
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+      react: eslintPluginReact,
+      "react-hooks": eslintPluginReactHooks,
+      "jsx-a11y": eslintPluginJsxA11y,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          singleQuote: true,
+          tabWidth: 2,
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
